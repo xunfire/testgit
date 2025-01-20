16ffc8a42b380ebe478e3d4452c5e4d3ff74913d
asher <asher@dynadot.com>
2025-01-20 14:18:27 +0800

    fixnewbug

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
   public static void main(
       String[] args) {//TIP 当文本光标位于高亮显示的文本处时按 <shortcut actionId="ShowIntentionActions"/>
     // 查看 IntelliJ IDEA 建议如何修正。
-    System.out.printf("Hello world");
+    System.out.printf("Hello this world");
     System.out.printf("testtest" + "master" + "test1");
     System.out.println("test2222222");
     System.out.println("newTestWeb");
@@ -29,6 +29,8 @@ public class Main {
       System.out.println("testDEV");
       System.out.println("hasjdkhasjkhdkjashdkj");
       System.out.println("fixbug");
+      System.out.println("newnewnenwenwnenwenwew");
+      System.out.println("fixnewbug");
     }
   }
 }
