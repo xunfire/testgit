4848ce346250d42975fb8c37fc5f1da85d1a7644
xunfire <jxfei2001@163.com>
2025-01-20 13:50:46 +0800

    addSomeMessage

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,6 +9,10 @@ public class Main {
     System.out.printf("Hello and welcome!");
     System.out.printf("testtest");
     System.out.println("testDEV");
+     System.out.println("testDEV");
+     System.out.println("testDEV");
+     System.out.println("testDEV");
+     System.out.println("testDEV");
     for (int i = 1; i <= 5; i++) {
       //TIP 按 <shortcut actionId="Debug"/> 开始调试代码。我们已经设置了一个 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 断点
       // 但您始终可以通过按 <shortcut actionId="ToggleLineBreakpoint"/> 添加更多断点。
