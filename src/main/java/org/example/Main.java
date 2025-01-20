e9510349e0e2e7a26e0896095ad0101240f233ed
asher <asher@dynadot.com>
2025-01-20 13:23:34 +0800

    master_test_to_dev

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,6 +9,7 @@ public class Main {
     System.out.printf("testtest"+"master"+"test1");
     System.out.println("test2222222");
     System.out.println("newTesttest");
+    System.out.println("newTestMasterToDEV");
     for (int i = 1; i <= 5; i++) {//TIP 按 <shortcut actionId="Debug"/> 开始调试代码。我们已经设置了一个 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 断点
       // 但您始终可以通过按 <shortcut actionId="ToggleLineBreakpoint"/> 添加更多断点。
       System.out.println("i = " + i);
